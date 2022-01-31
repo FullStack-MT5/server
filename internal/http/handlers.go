d08a3a57a800bf9b6dacf7c243920bfae0343ef5
moreirathomas <moreirathomas97@gmail.com>
2022-01-31 11:32:53 +0100

    feat: reuse http common helpers

--- internal/http/handlers.go
+++ internal/http/handlers.go
@@ -8,34 +8,22 @@ import (
 )
 
 func (s *Server) handlePostReport(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Content-Type", "application/json")
-
 	report := &internal.Report{}
 	if err := json.NewDecoder(r.Body).Decode(&report); err != nil {
-		rw.WriteHeader(400)
+		respondHTTPError(rw, errNotFound)
 		return
 	}
 
 	s.Repository.Report = report
 
-	rw.WriteHeader(201)
+	respondJSON(rw, 201, nil)
 }
 
 func (s *Server) handleGetReport(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Content-Type", "application/json")
-
 	if s.Repository.Report == nil {
-		rw.WriteHeader(404)
+		respondHTTPError(rw, errNotFound)
 		return
 	}
 
-	resp, err := json.Marshal(s.Repository.Report)
-
-	if err != nil {
-		rw.WriteHeader(500)
-		return
-	}
-
-	rw.Write(resp)
-	rw.WriteHeader(200)
+	respondJSON(rw, 200, s.Repository.Report)
 }
