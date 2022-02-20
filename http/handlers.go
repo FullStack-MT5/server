97eec97fb962759762d6689722164ec16d36f6a3
moreirathomas <moreirathomas97@gmail.com>
2022-02-20 20:09:02 +0100

    feat: call repository methods in server handlers

--- http/handlers.go
+++ http/handlers.go
@@ -1,13 +1,38 @@
 package http
 
 import (
+	"encoding/gob"
+	"fmt"
+	"io"
 	"net/http"
+
+	"github.com/benchttp/server"
 )
 
 func (s *Server) handlePostReport(rw http.ResponseWriter, r *http.Request) {
-	respondHTTPError(rw, errInternal)
+	b := server.Benchmark{}
+
+	err := gob.NewDecoder(r.Body).Decode(&b)
+	if err != nil && err != io.EOF {
+		fmt.Println(err)
+		respondHTTPError(rw, errBadRequest)
+		return
+	}
+
+	_, err = s.Repository.Create(r.Context(), b)
+	if err != nil {
+		respondHTTPError(rw, errInternal)
+		return
+	}
+	respondJSON(rw, 201, nil)
 }
 
 func (s *Server) handleGetReport(rw http.ResponseWriter, r *http.Request) {
-	respondHTTPError(rw, errInternal)
+	id := r.URL.Query().Get("id")
+	report, err := s.Repository.Retrieve(r.Context(), id)
+	if err != nil {
+		respondHTTPError(rw, errNotFound) // TODO differentiate not found and decoding
+		return
+	}
+	respondJSON(rw, 200, report)
 }
