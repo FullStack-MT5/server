d28dafb45a0c6943ff55a976708d70b3e2acd1f0
moreirathomas <moreirathomas97@gmail.com>
2022-03-01 12:50:59 +0100

    feat: set nosniff in json response too

--- write.go
+++ write.go
@@ -19,13 +19,13 @@ func writeError(w http.ResponseWriter, err error) {
 	// TODO verbose mode and/or logging to file.
 	log.Println(err.Error())
 
-	w.Header().Set("X-Content-Type-Options", "nosniff")
 	writeJSON(w, httpError{Message: message}, e.Code)
 }
 
 // writeJSON writes data to w in JSON format and sets the response satus code.
 func writeJSON(w http.ResponseWriter, data interface{}, code int) {
 	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("X-Content-Type-Options", "nosniff")
 	w.WriteHeader(code)
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
