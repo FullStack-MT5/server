d24aca686ec20633dacd8b3dc2820abbd2f465ee
C <>
2022-03-08 00:41:41 +0100

    Replace `report` by `stats`

--- computed_stats.go
+++ computed_stats.go
@@ -7,13 +7,13 @@ import (
 
 func (s *Server) ListMetadataByUserID(w http.ResponseWriter, r *http.Request) {
 	// TO DO: get userID from authentication to use it here instead of "1"
-	report, err := s.ComputedStatsService.ListMetadataByUserID(1)
+	stats, err := s.ComputedStatsService.ListMetadataByUserID(1)
 	if err != nil {
 		writeError(w, ErrNotFound.Wrap(err)) // TODO differentiate not found and decoding
 		return
 	}
 
-	writeJSON(w, report, 200)
+	writeJSON(w, stats, 200)
 }
 
 func (s *Server) FindComputedStatsByID(w http.ResponseWriter, r *http.Request) {
@@ -28,11 +28,11 @@ func (s *Server) FindComputedStatsByID(w http.ResponseWriter, r *http.Request) {
 		writeError(w, ErrBadRequest.Wrap(err))
 		return
 	}
-	report, err := s.ComputedStatsService.FindComputedStatsByID(idInt)
+	stats, err := s.ComputedStatsService.FindComputedStatsByID(idInt)
 	if err != nil {
 		writeError(w, ErrNotFound.Wrap(err)) // TODO differentiate not found and decoding
 		return
 	}
 
-	writeJSON(w, report, 200)
+	writeJSON(w, stats, 200)
 }
