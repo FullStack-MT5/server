949aa8027d9a3686223d4182a77ca8c8f4a22d41
C <>
2022-03-07 14:29:44 +0100

    Add some documentation comments in `repository.go`

--- benchttp/repository.go
+++ benchttp/repository.go
@@ -13,8 +13,17 @@ type ReportService interface {
 	Retrieve(ctx context.Context, id string) (Report, error)
 }
 
+// BenchmarkService defines the interface to implement by a
+// service facade inside the application.
 type BenchmarkService interface {
+	// Close closes the connection to the database storing the
+	// benchmarks data.
 	Close()
+	// ListMetadataByUserID retrieves all the metadata (Tag and
+	// FInishedAt) related to a User with the User id.
 	ListMetadataByUserID(id int) ([]*Metadata, error)
+	// FindBenchmarkDetailByID retrieves a benchmark and all
+	// its related details (metadata, codestats, timestats),
+	// provided a metadata id.
 	FindBenchmarkDetailByID(id int) (*Benchmark, error)
 }
