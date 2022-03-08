74d9b6a178c3d6885bbe768be57b42fb5db5f499
C <>
2022-03-08 01:08:03 +0100

    Add `getStringParamAndConvertToInt()` method in `computed_stats.go`

--- computed_stats.go
+++ computed_stats.go
@@ -17,17 +17,12 @@ func (s *Server) ListMetadataByUserID(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) FindComputedStatsByID(w http.ResponseWriter, r *http.Request) {
-	idString, err := pathParam(r, idParam)
+	idInt, err := getStringParamAndConvertToInt(r, idParam)
 	if err != nil {
 		writeError(w, ErrBadRequest.Wrap(err))
 		return
 	}
 
-	idInt, err := strconv.Atoi(idString)
-	if err != nil {
-		writeError(w, ErrBadRequest.Wrap(err))
-		return
-	}
 	stats, err := s.ComputedStatsService.FindComputedStatsByID(idInt)
 	if err != nil {
 		writeError(w, ErrNotFound.Wrap(err)) // TODO differentiate not found and decoding
@@ -35,4 +30,16 @@ func (s *Server) FindComputedStatsByID(w http.ResponseWriter, r *http.Request) {
 	}
 
 	writeJSON(w, stats, 200)
+}
+
+func getStringParamAndConvertToInt(r *http.Request, idParam param) (int, error) {
+	idString, err := pathParam(r, idParam)
+	if err != nil {
+		return 0, err
+	}
+	idInt, err := strconv.Atoi(idString)
+	if err != nil {
+		return 0, err
+	}
+	return idInt, nil
 }
