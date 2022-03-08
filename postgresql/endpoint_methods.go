7b545f236ac9a5f7237924d2bfc735fa8582285f
C <>
2022-03-08 01:51:03 +0100

    Make database retrieving code more readable by adding some line returns

--- postgresql/endpoint_methods.go
+++ postgresql/endpoint_methods.go
@@ -9,7 +9,7 @@ import (
 func (c computedStatsService) ListMetadataByUserID(userID int) ([]benchttp.Metadata, error) {
 	metadataList := []benchttp.Metadata{}
 
-	stmt, err := c.db.Prepare("SELECT tag, finished_at FROM metadata WHERE user_id = $1 ORDER BY finished_at DESC")
+	stmt, err := c.db.Prepare(`SELECT tag, finished_at FROM metadata WHERE user_id = $1 ORDER BY finished_at DESC`)
 	if err != nil {
 		return []benchttp.Metadata{}, ErrPreparingStmt
 	}
@@ -36,18 +36,45 @@ func (c computedStatsService) ListMetadataByUserID(userID int) ([]benchttp.Metad
 	return metadataList, nil
 }
 
-func (c *computedStatsService) FindComputedStatsByID(metadataID int) (*benchttp.ComputedStats, error) {
-	computedStats := &benchttp.ComputedStats{}
+func (c computedStatsService) FindComputedStatsByID(metadataID int) (benchttp.ComputedStats, error) {
+	computedStats := benchttp.ComputedStats{}
 
-	stmt := "SELECT m.tag, m.finished_at, c.code_1xx, c.code_2xx, c.code_3xx, c.code_4xx, c.code_5xx, t.min, t.max, t.mean, t.median, t.variance, t.deciles " +
-		"FROM public.metadata AS m " +
-		"INNER JOIN public.codestats AS c ON c.metadata_id = m.id " +
-		"INNER JOIN public.timestats AS t ON t.metadata_id = m.id " +
-		"WHERE m.id = $1 " +
-		"ORDER BY m.finished_at DESC"
+	stmt := `SELECT
+			m.tag,
+			m.finished_at,
+			c.code_1xx,
+			c.code_2xx,
+			c.code_3xx,
+			c.code_4xx,
+			c.code_5xx,
+			t.min,
+			t.max,
+			t.mean,
+			t.median,
+			t.variance,
+			t.deciles
+		FROM public.metadata AS m
+		INNER JOIN public.codestats AS c ON c.metadata_id = m.id
+		INNER JOIN public.timestats AS t ON t.metadata_id = m.id
+		WHERE m.id = $1
+		ORDER BY m.finished_at DESC`
 
 	row := c.db.QueryRow(stmt, metadataID)
-	err := row.Scan(&computedStats.Metadata.Tag, &computedStats.Metadata.FinishedAt, &computedStats.Codestats.Code1xx, &computedStats.Codestats.Code2xx, &computedStats.Codestats.Code3xx, &computedStats.Codestats.Code4xx, &computedStats.Codestats.Code5xx, &computedStats.Timestats.Min, &computedStats.Timestats.Max, &computedStats.Timestats.Mean, &computedStats.Timestats.Median, &computedStats.Timestats.Variance, (*pq.Float64Array)(&computedStats.Timestats.Deciles))
+	err := row.Scan(
+		&computedStats.Metadata.Tag,
+		&computedStats.Metadata.FinishedAt,
+		&computedStats.Codestats.Code1xx,
+		&computedStats.Codestats.Code2xx,
+		&computedStats.Codestats.Code3xx,
+		&computedStats.Codestats.Code4xx,
+		&computedStats.Codestats.Code5xx,
+		&computedStats.Timestats.Min,
+		&computedStats.Timestats.Max,
+		&computedStats.Timestats.Mean,
+		&computedStats.Timestats.Median,
+		&computedStats.Timestats.Variance,
+		(*pq.Float64Array)(&computedStats.Timestats.Deciles),
+	)
 	if err != nil {
 		return computedStats, ErrScanningRows
 	}
