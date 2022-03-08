5ae05a067eb79ec0c7bb31ea58364dfdd3b8affb
C <>
2022-03-08 14:56:00 +0100

    Get environment variables in main

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"log"
 	"os"
@@ -16,37 +17,66 @@ import (
 const defaultPort = "9998"
 
 func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run() error {
 	port := flag.String("port", defaultPort, "Address for the server to listen on.")
 	flag.Parse()
 	addr := ":" + *port
 
 	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatalf("Error loading .env file")
+		return errors.New("Error loading .env file")
 	}
 
 	projectID := os.Getenv("GOOGLE_PROJECT_ID")
 	if projectID == "" {
-		log.Fatalf("GOOGLE_PROJECT_ID variable is not defined")
+		return errors.New("GOOGLE_PROJECT_ID variable is not defined")
 	}
 
 	collectionID := os.Getenv("FIRESTORE_COLLECTION_ID")
 	if projectID == "" {
-		log.Fatalf("FIRESTORE_COLLECTION_ID variable is not defined")
+		return errors.New("FIRESTORE_COLLECTION_ID variable is not defined")
 	}
+
+	var psqlConfig postgresql.Config
+
+	psqlConfig.Host = os.Getenv("PSQL_HOST")
+	if psqlConfig.Host == "" {
+		return errors.New("could not find 'PSQL_HOST' environment variable")
+	}
+
+	psqlConfig.User = os.Getenv("PSQL_USER")
+	if psqlConfig.User == "" {
+		return errors.New("could not find 'PSQL_USER' environment variable")
+	}
+
+	psqlConfig.Password = os.Getenv("PSQL_PASSWORD")
+	if psqlConfig.Password == "" {
+		return errors.New("could not find 'PSQL_PASSWORD' environment variable")
+	}
+
+	psqlConfig.DBName = os.Getenv("PSQL_NAME")
+	if psqlConfig.DBName == "" {
+		return errors.New("could not find 'PSQL_NAME' environment variable")
+	}
+
+	psqlConfig.IdleConn = 10
+	psqlConfig.MaxConn = 25
 
 	rs, err := firestore.NewReportService(context.Background(), projectID, collectionID)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
-	cs, err := postgresql.NewComputedStatsService(10, 10)
+	cs, err := postgresql.NewComputedStatsService(psqlConfig)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	srv := server.New(addr, rs, cs)
-	if err := srv.Start(); err != nil {
-		log.Fatal(err)
-	}
+	return srv.Start()
 }
--- postgresql/connection.go
+++ postgresql/connection.go
@@ -2,29 +2,21 @@ package postgresql
 
 import (
 	"database/sql"
-	"errors"
 	"fmt"
-	"os"
 
 	_ "github.com/GoogleCloudPlatform/cloudsql-proxy/proxy/dialers/postgres" // blank import
-	"github.com/joho/godotenv"
 )
 
 type ComputedStatsService struct {
 	db *sql.DB
 }
 
-func NewComputedStatsService(idleConn, maxConn int) (ComputedStatsService, error) {
-	connectionInfo, err := getConnectionInfoFromEnvVariables()
-	if err != nil {
-		return ComputedStatsService{}, err
-	}
-
+func NewComputedStatsService(config Config) (ComputedStatsService, error) {
 	dbURI := fmt.Sprintf("host=%s user=%s password=%s dbname=%s sslmode=disable",
-		connectionInfo.host,
-		connectionInfo.user,
-		connectionInfo.password,
-		connectionInfo.dbName)
+		config.Host,
+		config.User,
+		config.Password,
+		config.DBName)
 
 	db, err := sql.Open("cloudsqlpostgres", dbURI)
 	if err != nil {
@@ -36,54 +28,17 @@ func NewComputedStatsService(idleConn, maxConn int) (ComputedStatsService, error
 		return ComputedStatsService{}, ErrDatabasePing
 	}
 
-	db.SetMaxIdleConns(idleConn)
-	db.SetMaxOpenConns(maxConn)
+	db.SetMaxIdleConns(config.IdleConn)
+	db.SetMaxOpenConns(config.MaxConn)
 
 	return ComputedStatsService{db}, nil
 }
 
-type connectionInfo struct {
-	host     string
-	user     string
-	password string
-	dbName   string
-}
-
-func getConnectionInfoFromEnvVariables() (connectionInfo, error) {
-	envVariablesErrors := []error{}
-
-	appendError := func(err error) {
-		envVariablesErrors = append(envVariablesErrors, err)
-	}
-
-	err := godotenv.Load(".env")
-	if err != nil {
-		appendError(errors.New("could not load '.env' file"))
-	}
-
-	host := os.Getenv("PSQL_HOST")
-	if host == "" {
-		appendError(errors.New("could not find 'HOST' environment variable"))
-	}
-
-	user := os.Getenv("PSQL_USER")
-	if user == "" {
-		appendError(errors.New("could not find 'DB_USER' environment variable"))
-	}
-
-	password := os.Getenv("PSQL_PASSWORD")
-	if password == "" {
-		appendError(errors.New("could not find 'DB_PASSWORD' environment variable"))
-	}
-
-	dbName := os.Getenv("PSQL_NAME")
-	if dbName == "" {
-		appendError(errors.New("could not find 'DB_NAME' environment variable"))
-	}
-
-	if len(envVariablesErrors) > 0 {
-		return connectionInfo{}, &ErrNotFoundEnvVariables{envVariablesErrors}
-	}
-
-	return connectionInfo{host, user, password, dbName}, nil
+type Config struct {
+	Host     string
+	User     string
+	Password string
+	DBName   string
+	IdleConn int
+	MaxConn  int
 }
